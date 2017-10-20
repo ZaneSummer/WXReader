23f85dfe2be86ebefce6126fbe0150519906d081
zhaowenchao <zhao.wenchao@jyall.com>
2017-10-20 17:08:15 +0800

    init

---
+++ pages/posts/post.js
@@ -0,0 +1,18 @@
+var postData = require('../../data/post-data.js');
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+   
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({ postkey: postData.postList});
+  },
+
+})
