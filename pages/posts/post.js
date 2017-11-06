6519cd434919c709b9e8bb7364b3989305510344
zhaowenchao <zhao.wenchao@jyall.com>
2017-11-06 19:09:35 +0800

    list addd event

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -5,14 +5,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-   
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({ postkey: postData.postList});
+    this.setData({ postkey: postData.postList });
   },
+
+  onItemTap: function (event) {
+    var item = event.currentTarget.dataset.object;
+    console.log(item.date);
+    wx.navigateTo({
+      url: '../posts/post-detail/post-detail',
+    })
+  }
 
 })
